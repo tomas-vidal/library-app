7f99c1e5e34fc1b1aa30fc01486a81c94b2eeb54
tomas-vidal <vidalltomas@gmail.com>
2023-03-13 17:56:24 -0300

    do all logic and styling

--- script.js
+++ script.js
@@ -5,19 +5,39 @@ let author = document.querySelector("#author");
 let totalPages = document.querySelector("#pages");
 let read = document.querySelector("#read");
 let id = 0;
+let editingId;
 let myBooks = [];
+let editMode = false;
+let currentIndex;
 
 function Book(title, author, totalPages, id) {
   (this.title = title),
     (this.author = author),
     (this.totalPages = totalPages),
-    (this.id = id);
+    (this.id = id),
+    (this.read = read.checked);
 }
 
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
+  console.log(id);
 
   if (title.value === "" || author.value === "" || totalPages === "") {
+    return;
+  }
+
+  if (editMode) {
+    myBooks[currentIndex] = {
+      title: title.value,
+      author: author.value,
+      totalPages: totalPages.value,
+      id: editingId,
+      read: read.checked,
+    };
+    resetInputs();
+    displayBooks(myBooks);
+    editMode = false;
+    submitButton.textContent = "ADD BOOK";
     return;
   }
 
@@ -28,9 +48,7 @@ submitButton.addEventListener("click", (e) => {
   displayBooks(myBooks);
 
   /* RESETS THE INPUTS AND REFRESH ID */
-  title.value = "";
-  author.value = "";
-  totalPages.value = "";
+  resetInputs();
   id++;
 });
 
@@ -42,19 +60,53 @@ const displayBooks = (bookArray) => {
           <h2>${book.title}</h2>
           <p>${book.author}</p>
           <span>${book.totalPages}</span>
-          <span>SI</span>
-          <img
-                  src="./images/trash-can-outline.svg"
-                  alt=""
-                  class="app__book-delete"
-                  onclick="deleteBook(${book.id})"
-                />
+          <span>${book.read ? "Read" : "Not read"}</span>
+          <div class="app__options-book">
+            <img
+                    src="./images/trash-can-outline.svg"
+                    alt=""
+                    class="app__book-delete"
+                    onclick="deleteBook(${book.id})"
+                  />
+            <img src="./images/pencil.svg" alt="" onclick="editBook(${
+              book.id
+            })"class="app__book-edit" />
+          </div>
         </div>`;
   });
 };
 
 const deleteBook = (id) => {
+  if (editMode) {
+    editMode = false;
+    submitButton.textContent = "ADD BOOK";
+    resetInputs();
+  }
   myBooks = myBooks.filter((book) => book.id !== id);
-  console.log(myBooks);
   displayBooks(myBooks);
+};
+
+const editBook = (id) => {
+  submitButton.textContent = "EDIT CONFIRM";
+  editingId = id;
+  currentIndex = myBooks.findIndex((book) => book.id === id);
+  editMode = true;
+  title.value = myBooks[currentIndex].title;
+  author.value = myBooks[currentIndex].author;
+  totalPages.value = myBooks[currentIndex].totalPages;
+  read.checked = myBooks[currentIndex].read;
+
+  document.querySelector(`[data-id="${editingId}"]`).style.backgroundColor =
+    "#6036b3";
+  document.querySelector(`[data-id="${editingId}"]`).style.color = "#fff";
+  document
+    .querySelector(`[data-id="${editingId}"]`)
+    .querySelector(".app__options-book").style.filter = "invert(1)";
+};
+
+const resetInputs = () => {
+  title.value = "";
+  author.value = "";
+  totalPages.value = "";
+  read.checked = false;
 };
